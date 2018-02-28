c8c1d82aa94bc205bdf48612a7227987327e6728
Kathan Vyas <30324812+kathanvyas@users.noreply.github.com>
2018-02-28 02:18:40 -0500

    Create Custom_image.py

---
+++ Custom_image.py
@@ -0,0 +1,12 @@
+
+my_image = "my_image.jpg"   # change this to the name of your image file 
+# We preprocess the image to fit your algorithm.
+fname = "images/" + my_image
+image = np.array(ndimage.imread(fname, flatten=False))
+my_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1, num_px*num_px*3)).T
+my_predicted_image = predict(d["w"], d["b"], my_image)
+
+plt.imshow(image)
+print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
+
+
